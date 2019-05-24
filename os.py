55d59d6a550451d62496424531e650ceabd51e36
pkonieczny007 <pkonieczny007@gmail.com>
2019-05-24 12:40:18 +0200

    exception try adding

--- os.py
+++ os.py
@@ -4,11 +4,14 @@ import fdxf
 n=0
 
 for filename in os.listdir():
-    if filename.endswith('.dxf'):
-        n+=1
-        wys=fdxf.wys(filename)
-        szer=fdxf.sze(filename)
-        print(n, filename, wys, szer)
+    try:
+        if filename.endswith('.dxf'):
+            n+=1
+            wys=fdxf.wys(filename)
+            szer=fdxf.sze(filename)
+            print(n, filename, wys, szer)
+    except:
+        pass
         
 
 
