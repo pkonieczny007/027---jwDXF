483b52869d5c08356e5bc35caf69a2e506c15fab
pkonieczny007 <50171535+pkonieczny007@users.noreply.github.com>
2019-08-15 22:54:34 +0200

    adding excel value writter

--- os.py
+++ os.py
@@ -1,18 +1,43 @@
 import os 
 import fdxf
+import openpyxl 
 
 n=0
+index_dict={}
+idi= index_dict
+test_dict = {}
+
+
+wb = openpyxl.Workbook()    #utworzenie nowego pliku excel
+ws = wb.active
 
 for filename in os.listdir():
-    try:
-        if filename.endswith('.dxf'):
-            n+=1
-            wys=fdxf.wys(filename)
-            szer=fdxf.sze(filename)
-            print(n, filename, wys, szer)
-    except:
-        print(n, filename, 'ERROR')
-        
+    if filename.endswith('.dxf'):
+        n+=1
+        wys=fdxf.wys(filename)
+        szer=fdxf.sze(filename)
+        #print(n, filename, wys, szer)
+        test_dict[filename]=(wys,szer)
+        index_dict[n]=filename
+        a='A'+str(n)
+        b='B'+str(n)
+        c='C'+str(n)
+        ws[a]=filename
+        ws[b]=wys
+        ws[c]=szer
+    else:
+        print('dane zaladowane')
+
+
+
+
+
+    
+
+
+
+wb.save('testing_tech.xlsx')
+print('plik testowy zapisany;')
 
 
         
